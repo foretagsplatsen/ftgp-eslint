8cd626b00fd765aac983fa1b01fcac2a6c1925ac
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-03-09 15:48:19 +0100

    Add a rule to ensure the dependency order in the define callback

---
+++ tests/lib/rules/ensure-dependency-order.js
@@ -0,0 +1,64 @@
+/**
+ * @fileoverview Ensure the dependencies are loaded in the correct order
+ * @author Benjamin Van Ryseghem
+ * @copyright 2016 Benjamin Van Ryseghem. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/ensure-dependency-order"),
+
+	RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("ensure-dependency-order", rule, {
+
+	valid: [
+		{
+			code: "define()"
+		},
+		{
+			code: "define(['foo'])"
+		},
+		{
+			code: "define(function(){})"
+		},
+		{
+			code: "define(['foo/bar'], function(bar){})"
+		},
+		{
+			code: "define(['foo/bar', 'foo'], function(bar){})"
+		},
+		{
+			code: "define(['foo/bar'], function(barModel){})"
+		},
+		{
+			code: "define(['jquery'], function(jQuery){})",
+			options: [{jquery: "jQuery"}]
+		}
+	],
+
+	invalid: [
+		{
+			code: "define(['foo', 'bar'], function(bar, foo){})",
+			errors: [
+				{
+					message: 'Expected name is "foo" but "bar".',
+					type: "Identifier"
+				},
+				{
+					message: 'Expected name is "bar" but "foo".',
+					type: "Identifier"
+				}
+			]
+		}
+	]
+});
